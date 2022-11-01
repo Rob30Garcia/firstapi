1068e51e0982256958c5d1034ac77ae04f10c69e
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-11-01 08:07:16 -0400

    post route

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
+//middleware
+app.use(express.json());
+
 app.route('/').get(
   (req, res) => res.send('hello')
+);
+
+app.route('/').post(
+  (req, res) => res.send(req.body)
 );
 
 app.listen(3000);
